bf13ba70c01267a25f89af8f31510e5a458ae256
Vivian <95883554+VivianPolygon@users.noreply.github.com>
2026-04-30 15:23:33 -0700

    Complete
    Optimized, Valid Algorithm
    All Clear!

--- floats-v2/firmware/src/asmFmax.s
+++ floats-v2/firmware/src/asmFmax.s
@@ -79,26 +79,20 @@ nanValue: .word 0x7FFFFFFF
 .global initVariables
  .type initVariables,%function
 initVariables:
-    /* YOUR initVariables CODE BELOW THIS LINE! Don't forget to follow the calling convention! */
-    push {r4-r11,LR}
+    push {r4-r11,LR} /* perserve upper registers, and link register (calling convention 1) */
     
     /* because the variables we need to initialize are consecutive in memory, we can loop over them similar to an array for initialization */
     LDR r11, =0 /* initialization value */
-    LDR r10, =15 /* consecutive words to initialize */
+    LDR r10, =14 /* consecutive words to initialize - 1, PL condition code that checks this includes 0 */
     LDR r9, =f0 /* starting address */
     
     initLoop:
-    STR r11, [r9] /* store value '0' at current address */
-    ADD r9, r9, 4 /* add 4 to adress to move to next word (STR [r9], 4 does not seem to work) */
+    STR r11, [r9], 4 /* store value '0' at current address, post-increment by 4 to hit next address (all data are alligned words) */
     SUBS r10, r10, 1 /* subtract 1 from the initialization count and update flags */
-    BEQ initVariablesComplete /* if the count is at 0, we are done, branch to done */
-    BPL initLoop /* if the count is greater than 0, continue the loop. PL includes 0, but EQ is checked first. If the count becomes negative somehow, should still fall through to done */
+    BPL initLoop /* if the count is greater than or equal to 0, continue the loop. PL includes 0 */
 
-    initVariablesComplete:
-    pop {r4-r11,LR}
-    MOV PC, LR
-    /* YOUR initVariables CODE ABOVE THIS LINE! Don't forget to follow the calling convention! */
-
+    pop {r4-r11,LR} /* restore upper registers, and link register (calling convention 2) */
+    MOV PC, LR /* move the link register into the program counter to jump back to the caller (calling convention 3) */
     
 /********************************************************************
  function name: getSignBit
@@ -112,19 +106,14 @@ initVariables:
 .global getSignBit
 .type getSignBit,%function
 getSignBit:
-    /* YOUR getSignBit CODE BELOW THIS LINE! Don't forget to follow the calling convention! */
-    push {r4-r11,LR}
+    push {r4-r11,LR} /* perserve upper registers, and link register (calling convention 1) */
     
     LDR r0, [r0] /* load from input */
     LSR r0, r0, 31 /* shift the register to the right, moving the sign bit into the LSB, and zeroing out the rest */
     STR r0, [r1] /* store the shifted registed to the specified memory location */
     
-    pop {r4-r11,LR}
-    MOV PC, LR
-    
-    /* YOUR getSignBit CODE ABOVE THIS LINE! Don't forget to follow the calling convention! */
-    
-
+    pop {r4-r11,LR} /* restore upper registers, and link register (calling convention 2) */
+    BX LR /* branch with exchange to use whats in the link register to move back to the caller (calling convention 3) */
     
 /********************************************************************
  function name: getExponent
@@ -141,8 +130,7 @@ getSignBit:
 .global getExponent
 .type getExponent,%function
 getExponent:
-    /* YOUR getExponent CODE BELOW THIS LINE! Don't forget to follow the calling convention! */
-    push {r4-r11,LR}
+    push {r4-r11,LR} /* perserve upper registers, and link register (calling convention 1) */
     
     LDR r0, [r0] /* load from input */
     /* The exponent is stored in bits 23 - 30 */
@@ -150,11 +138,8 @@ getExponent:
     AND r0, r0, 0xFF /* excludes sign bit */
     SUB r1, r0, 127 /* unbiased exponent */
     
-    pop {r4-r11,LR}
-    BX LR
-    /* YOUR getExponent CODE ABOVE THIS LINE! Don't forget to follow the calling convention! */
-   
-
+    pop {r4-r11,LR} /* restore upper registers, and link register (calling convention 2) */
+    BX LR /* branch with exchange to use whats in the link register to move back to the caller (calling convention 3) */
     
 /********************************************************************
  function name: getMantissa
@@ -168,8 +153,7 @@ getExponent:
 .global getMantissa
 .type getMantissa,%function
 getMantissa:
-    /* YOUR getMantissa CODE BELOW THIS LINE! Don't forget to follow the calling convention! */
-    push {r4-r11,LR}
+    push {r4-r11,LR} /* perserve upper registers, and link register (calling convention 1) */
     
     LDR r0, [r0] /* load from input */
     /* mantissa is in bits 0 - 22 */
@@ -178,12 +162,8 @@ getMantissa:
     LDR r1, =0x800000 /* implied bit */
     ADD r1, r0, r1 /* adds implied bit */
     
-    pop {r4-r11,LR}
-    MOV PC, LR
-    /* YOUR getMantissa CODE ABOVE THIS LINE! Don't forget to follow the calling convention! */
-   
-
-
+    pop {r4-r11,LR} /* restore upper registers, and link register (calling convention 2) */
+    MOV PC, LR /* move the link register into the program counter to jump back to the caller (calling convention 3) */
     
 /********************************************************************
  function name: asmIsZero
@@ -198,8 +178,7 @@ getMantissa:
 .global asmIsZero
 .type asmIsZero,%function
 asmIsZero:
-    /* YOUR asmIsZero CODE BELOW THIS LINE! Don't forget to follow the calling convention! */
-    push {r4-r11,LR}
+    push {r4-r11,LR} /* perserve upper registers, and link register (calling convention 1) */
     
     LDR r11, [r0] /* load from input */
     /* for a float to be zero, all bits except the sign bit must be cleared. */
@@ -207,15 +186,11 @@ asmIsZero:
     LSLS r10, r11, 1 /* shift out the sign bit, if the remaining value is not zero, the float is not 0 */
     MOVNE r0, 0 /* exponent and mantissa is not 0 */
     MOVEQ r0, 1 /* exponent and mantissa is 0 */
-    LSRS r10, r11, 31 /* isolate the sign bit and update flags. Could use previous function, however this is more compact */ 
+    LSRS r10, r11, 31 /* isolate the sign bit from initial load's value, and update flags. Could use getSignBit, however this is more compact */ 
     NEGNE r0, r0 /* if the shift that isolates the sign bit is not 0 (negative) negate. will change r0 from 1 to -1, but not affect 0 */
     
-    pop {r4-r11,LR}
-    BX LR
-    /* YOUR asmIsZero CODE ABOVE THIS LINE! Don't forget to follow the calling convention! */
-   
-
-
+    pop {r4-r11,LR} /* restore upper registers, and link register (calling convention 2) */
+    BX LR /* branch with exchange to use whats in the link register to move back to the caller (calling convention 3) */
     
 /********************************************************************
  function name: asmIsInf
@@ -230,17 +205,17 @@ asmIsZero:
 .global asmIsInf
 .type asmIsInf,%function
 asmIsInf:
-    /* YOUR asmIsInf CODE BELOW THIS LINE! Don't forget to follow the calling convention! */
-    push {r4-r11,LR}
+    push {r4-r11,LR} /* perserve upper registers, and link register (calling convention 1) */
     
     MOV r11, r0 /* functions are used that overwrite r0, store input in a register thats perserved by calling convention */
     LDR r10, [r0] /* load from input */
+    
     /* for a float to be infinity, all bits in the exponent must be set, while all bits in the mantissa must be cleared */
-    /* the sign bit may or may not be set. If its set its positive infinity. If it is negative, its negative infinity */
+    /* the sign bit may or may not be set. If the sign is positive, its positive infinity. If the sign is negative, its negative infinity */
     
     BL getMantissa /* function call to get the mantissa, stores it in r0, r1 is the mantissa with the implied bit, which we do not need here*/
     MOVS r9, r0 /* mantissa */ 
-    MOVNE r0, 0 /* the mantissa has some bits set, so the value is not infinity, thefore the output will be 0 */
+    MOVNE r0, 0 /* the mantissa has some bits set, so the value is not infinity, therefore the output will be 0 */
     BNE asmIsInfComplete /* we already know the output due to the mantissa */
     /* mantissa was 0, so we need to look at the exponent */
     MOV r0, r11 /* assign float memory location to r0 for function */
@@ -249,17 +224,13 @@ asmIsInf:
     MOVNE r0, 0 /* the exponent is not all set, so we know the value is not infinity */
     BNE asmIsInfComplete /* we already know the output due to the exponent */
     /* at this point we know that the float is infinity, we now need to determine it's sign */
-    LSRS r0, r10, 31 /* isolates the sign bit at the bottom of the register and updates flags */
+    LSRS r0, r10, 31 /* isolates the sign bit at the bottom of the register and updates flags. We could use getSignBit, but this is more compact */
     MOVEQ r0, 1  /* if the signbit is positive, return  1, as specified */
     MOVNE r0, -1 /* if the signbit is negative, return -1, as specified */
     
     asmIsInfComplete:
-    pop {r4-r11,LR}
-    BX LR
-    /* YOUR asmIsInf CODE ABOVE THIS LINE! Don't forget to follow the calling convention! */
-   
-
-
+    pop {r4-r11,LR} /* restore upper registers, and link register (calling convention 2) */
+    BX LR /* branch with exchange to use whats in the link register to move back to the caller (calling convention 3) */
     
 /********************************************************************
 function name: asmFmax
@@ -292,35 +263,34 @@ where:
 .global asmFmax
 .type asmFmax,%function
 asmFmax:   
-    /* r0 input is f0 (floating-point value) */
-    /* r1 input is f1 (floating-point value) */
-    /* r0 output is address of fmax */
-    push {r4-r11,LR}   
-    BL initVariables /* initialize all variables to 0. Only write to fMax, function dosen't interact out f0 and f1 blocks after init */
+    push {r4-r11,LR} /* perserve upper registers, and link register (calling convention 1) */ 
+    BL initVariables /* initialize all variables to 0. function dosen't interact completly with f0 and f1 blocks after init */
+    /* f0, sb0, f1, and sb1 are written to, other memoru addresses for f0 and f1 blocks are not */
     
     /* input processing */
     LDR r2, =f0 /* assign the designated location in memory the inputted first floating-point operand */
     STR r0, [r2]
-    LDR r2, =f1 /* assign the designated location in memory the inputted second floating-point operand*/
-    STR r1, [r2]
+    LDR r3, =f1 /* assign the designated location in memory the inputted second floating-point operand*/
+    STR r1, [r3]
      
     /* check sign difference. Individual signs stored in r10 and r11. sign comparison is stored in r9 */
     
     /* compute the sign of the first input */
-    LDR r0, =f0
-    LDR r1, =sb0
+    MOV r0, r2 /* move the address of f0 to r0, for getSignBit input */
+    LDR r1, =sb0 /* r1 needs to be the writing address, for getSignBit */
     BL getSignBit /* INPUTS: r0 - [float input], r1 - address of output, OUTPUT: r1, address of output (unchanged) */
-    LDR r10, [r1] /* get sign for later */
+    LDR r10, [r1] /* get sign for later, store in specified address */
     /* compute the sign of the second input */
-    LDR r0, =f1
-    LDR r1, =sb1
+    MOV r0, r3 /* move the address of f1 to r0, for getSignBit input */
+    LDR r1, =sb1 /* r1 needs to be the writing address, for getSignBit */
     BL getSignBit /* INPUTS: r0 - [float input], r1 - address of output, OUTPUT: r1, address of output (unchanged) */
-    LDR r11, [r1] /* get sign for later */
+    LDR r11, [r1] /* get sign for later, store in specified address */
     
     /* we need to check the signs against eachother. there are four distinct cases we need to consider */
+    /* to get 4 cases from two bits, we can weight one (double it), and add them together. Then we can read this value */
     ADD r8, r10, r10 /* double the sign value for f0, allows for sorting into four cases from signs, can reconize sign order */
     ADD r8, r8, r11 /* add to the doubled f0 sign the sign of f1 */
-    /* below a pattern reminiscent of an enum and case statement is used to sort out if one is positve and one is negative,
+    /* below a pattern reminiscent of an enum and case statement is used to sort out which is which if one is positve and one is negative,
     or if they share a sign. in the former case, we can immediatly return, in the latter more steps are required, but we also
     need to remember what the sign that was shared is */
     TEQ r8, 1       /* case: 1  */
@@ -331,50 +301,48 @@ asmFmax:
     BEQ assignFloat /* break;   */  /* if our sign comparison results in a 2, f0 is negative, and f1 is positive. f1 is larger */   
     MOV r9, 0  /* after this point, we know the signs match, we need to note what they are though. line defaults the shared sign to positive */
     TEQ r8, 3       /* case: 3  */
-    MOVEQ r9, 1     /* load 1 into r9, indicates that the sign is negative */
+    MOVEQ r9, 1     /* load 1 into r9, indicates that the sign is negative. Note that r9 is the previously specified register for the sign comparison */
      
-    /* signs match, exponent and mantissa need to be directly checked */
+    /* signs match, exponent and mantissa need to be directly checked (jumping to assignFloat skips this section)*/
     
-    /* compare exponent and mantissam (r10 - f0, r1 - f11) */
-    LDR r10, =f0 /* get float addresses */
-    LDR r11, =f1
+    /* compare exponent and mantissa (r10 - f0, r1 - f11, r10 and r11 no longer need the individual signs, as r9 holds the relevant information on their shared sign) */
+    LDR r10, =f0 /* get float address for f0 */
+    LDR r11, =f1 /* get float address for f1 */
 	
     /* pseudo-delegates! */
-    LDR r6, =getExponent /* get the instruction address of get exponent into a register (we can swap this out with getMantissa later and reuse subsequent code) */
-    MOV r7, 0 /* this is used to prevent infinite loops, counter */
-    MOV r8, PC /* store this line into the program counter so that we can return later */
-    /* get f0 exponent (1st run), get f0 mantissa (2nd run) */
-
+    LDR r6, =getExponent /* get the instruction address of getExponent into a register (we can swap this out with getMantissa later and reuse subsequent code!) */
+    MOV r8, PC /* stores the line after the next into the Program Counter. Confirmed that like 318 is what gets stored through debugging techniques*/
+    MOV r7, 0 /* this is used to prevent infinite loops, lets us know what pass we are on */
+    
+    /* get f0 exponent (1st pass), get f0 mantissa (2nd pass) */
     MOV r0, r10 /* move f0's address into r0, both getExponent and getMantissa use this as their input */
     BLX r6 /* on the first pass, this is getExponent, on the second, getMantissa */
     MOV r4, r0 /* move into perserved register */
-    /* get f1 exponent (1st run), get f1 mantissa (2nd run) */
+    /* get f1 exponent (1st pass), get f1 mantissa (2nd pass) */
     MOV r0, r11 /* move f1's address into r0, both getExponent and getMantissa use this as their input */
     BLX r6 /* on the first pass, this is getExponent, on the second, getMantissa */
-    MOV r5, r0 /* move into perserved register */
-	
-    /* position float addresses for sharedSignApply */
-    MOV r0, r10
-    MOV r1, r11
 
     /* compare the values of the exponents to eachother (first pass), and mantissas (second pass) */
-    CMP r4, r5
-    MOVPL r2, 0 /* indicates that f0 is larger, or they are identical */
-    MOVMI r2, 1 /* indicates that f1 is larger */
+    CMP r4, r0 /* compare function outputs to see which is larger, r0 has f1's exponent or mantissa from function call */
+    MOVPL r2, 0 /* indicates that f0 is larger (absolute value), or they are identical */
+    MOVMI r2, 1 /* indicates that f1 is larger (absolute value) */
     BNE sharedSignApply /* if there is a disparity, we know one is larger, branch out. Will exit after first pass if exponent is larger */
 	
     CMP r7, 1 /* r7 will be 1 at the end of the second pass. If this is hit, the floats are identical. */
-    BEQ sharedSignApply /* exit, if this wasen't here, we would run the second pass over and over again if the floats are identical */
+    BEQ sharedSignApply /* exit, if this wasn't here, we would run the second pass over and over again if the floats are identical */
 	
     LDR r6, =getMantissa /* for our second pass, we want to use getMantissa instead of getExponent, so assign it to r6 */
     MOV r7, 1 /* allows us to exit in the edgecase of identical floats */
     MOV PC, r8 /* moves us back to the top of this section */
 	
-	/* apply shared sign flip to comparison, then sets */
-	/* f0's address should be in r10, f1's address should be in r11 */
-	/* r2 should contain a 0 if f0 has a greater abs, and a 1 if it has a smaller abs */
-	/* r9 should contain a 0 if the shared sign is positive, and a 1 if the shared sign is negative */
+    /* apply shared sign flip to comparison, then sets */
+    /* f0's address should be in r10, f1's address should be in r11 */
+    /* r2 should contain a 0 if f0 has a greater abs, and a 1 if it has a smaller abs */
+    /* r9 should contain a 0 if the shared sign is positive, and a 1 if the shared sign is negative */
     sharedSignApply:
+        /* position float addresses for assignFloat */
+	MOV r0, r10 /* r0 becomes f0 */
+	MOV r1, r11 /* f1 becomes f1 */
 	/* swap float positions before compare to invert result to map abs comparison to actual value comparison */
 	EORS r9, r2 /* by default, point to f0 *//* if both are negative, flip to f1 */ /* if f1 is larger, flip to f1 */ /* if both, no change (f0) */ 
 	MOVNE r2, r1 /* swap r0 and r1 */
@@ -385,14 +353,13 @@ asmFmax:
 	/* put address of float in r0 */
     assignFloat:
 	MOV r11, r0 /* store float address out of way */
-	BL getSignBit
-	LDR r7, [r1] /* gets sign bit of float into r7 for setOutput */
-	
-	MOV r0, r11 /* recall float address */
-	BL getExponent
+	/* sign bit */
+	LDR r7, [r0, 4] /* gets sign bit of float into r7 for setOutput. We always stored this, it is always the float address + 4, due to order of data up top */
+	/* exponent */
+	BL getExponent /* exponent address is still in r0 */
 	MOV r8, r0 /* gets stored exponent into r8, for setOutput */
-	MOV r9, r1 /* gets unbiased exponent into r9, for setOutput */
-	
+	MOV r9, r1 /* gets unbiased exponent into r9, for setOutput */	
+	/* mantissa */
 	MOV r0, r11 /* recall float address */
 	BL getMantissa
 	MOV r10, r1 /* gets mantissa (with implied bit) and stores into r10, for setOutput */
@@ -401,14 +368,12 @@ asmFmax:
 	MOVEQ r10, r0 /* overwrite with mantissa with no implied bit from earlier function return */
 	CMP r8, 0xFF /* stored exponent is all set */
 	MOVEQ r10, r0 /* overwrite with mantissa with no implied bit from earlier function return */
-	
+	/* float */
 	LDR r6, [r11] /* puts float into r6, for setOutput */
-        B setOutput
-	
 	
     setOutput: /* set the memory locations for the functions output, see below for what should be stored where */
-	LDR r11, =fMax
-	STR r6, [r11] /* the maximum float, in r6 */
+	LDR r0, =fMax /* r0 must be pointer to fmax for C call */
+	STR r6, [r0] /* the maximum float, in r6 */
 	LDR r11, =sbMax
 	STR r7, [r11] /* the maximum float's sign bit, in r7 */
 	LDR r11, =storedExpMax
@@ -417,13 +382,8 @@ asmFmax:
 	STR r9, [r11] /* the maximum float's stored exponent (unbiased), in r9 */
 	LDR r11, =mantMax
 	STR r10, [r11] /* the maximum float's mantissa in r10 */
-
 	
-    LDR r0, =fMax /* address of fmax, required for C call  */
-    pop {r4-r11,LR}
-    BX LR
-
-   
+    pop {r4-r11,PC} /* restore upper registers, and set program counter to caller's link (calling convention 2, 3) */ /* spite! */
 
 /**********************************************************************/   
 .end  /* The assembler will not process anything after this directive!!! */
